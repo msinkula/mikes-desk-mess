7999be046fdeeb009f33d01b59d06d70970d4908
Mike Sinkula <mike@mikesinkula.com>
2013-07-31 14:55:18 -0700

    display for test posting only

--- single.php
+++ single.php
@@ -7,7 +7,8 @@
 <article class="post" id="post-<?php the_ID(); ?>">
     <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
     <p class="postdata">Posted <?php the_time('M j, Y') ?> in <?php the_category(', ') ?></p>
-    <?php /*if(is_single('1420')):*/ ?> 
+    <?php if(is_single('1420')): // display for test posting only ?> 
+    
     <?php // if there is more than one attachment image, display as a flexslider gallery
 	
 	$attachments = get_children( array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image','caption' => $attachment->post_excerpt,) );
@@ -17,9 +18,11 @@
         <!-- Begin Slider --> 
         <div class="flexslider">
             <ul class="slides">
+            
             <?php // create the list items for images with captions
             
 			foreach ( $attachments as $attachment_id => $attachment ) {
+				
 				echo '<li>';
 				echo wp_get_attachment_image($attachment_id, 'large');
 				echo '<p>';
@@ -33,9 +36,9 @@
         </div>
         <!-- End Slider -->
         
-		<?php } // end count ?>
+	<?php } // end count attachements ?>
         
-    <?php /*endif;*/ /* End Post 1420*/ ?>
+    <?php endif; /* End Post 1420*/ ?>
     <?php the_content(''); ?> 
     <nav class="post-navigation">
         <span class="post-navigation-previous"><?php previous_post(' &laquo; %','', 'yes'); ?></span>
