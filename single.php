83e4d17117ca4140bbe649b2ff6c30e0bfb095ac
Mike Sinkula <mike@mikesinkula.com>
2013-07-31 14:44:16 -0700

    Count the images.

--- single.php
+++ single.php
@@ -7,26 +7,35 @@
 <article class="post" id="post-<?php the_ID(); ?>">
     <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
     <p class="postdata">Posted <?php the_time('M j, Y') ?> in <?php the_category(', ') ?></p>
-    
-    <?php if(is_single('1420')): ?> 
-    <!-- Begin Slider --> 
-    <div class="flexslider">
-        <ul class="slides">
-        <?php
-            $attachments = get_children( array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image','caption' => $attachment->post_excerpt,) );
-            foreach ( $attachments as $attachment_id => $attachment ) {
-                echo '<li>';
-                echo wp_get_attachment_image($attachment_id, 'large');
+    <?php /*if(is_single('1420')):*/ ?> 
+    <?php // if there is more than one attachment image, display as a flexslider gallery
+	
+	$attachments = get_children( array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image','caption' => $attachment->post_excerpt,) );
+	
+	if(count($attachments) > 1) { // count to see if there is more than one image ?>
+        
+        <!-- Begin Slider --> 
+        <div class="flexslider">
+            <ul class="slides">
+            <?php // create the list items for images with captions
+            
+			foreach ( $attachments as $attachment_id => $attachment ) {
+				echo '<li>';
+				echo wp_get_attachment_image($attachment_id, 'large');
 				echo '<p>';
 				echo get_post_field('post_excerpt', $attachment->ID);
 				echo '</p>';
 				echo '</li>';
 				
-            } ?>
-        </ul>
-    </div>
-    <!-- End Slider -->
-    <?php endif; ?>
+            } // end create list ?>
+            
+            </ul>
+        </div>
+        <!-- End Slider -->
+        
+		<?php } // end count ?>
+        
+    <?php /*endif;*/ /* End Post 1420*/ ?>
     <?php the_content(''); ?> 
     <nav class="post-navigation">
         <span class="post-navigation-previous"><?php previous_post(' &laquo; %','', 'yes'); ?></span>
