5b881cd86ae654122687426cafd6a0cafeea1a57
Mike Sinkula <mike@mikesinkula.com>
2013-08-03 09:50:54 -0700

    Change.

--- single.php
+++ single.php
@@ -6,14 +6,12 @@
 <?php while (have_posts()) : the_post(); ?>
 <article class="post" id="post-<?php the_ID(); ?>">
     <h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-    <p class="postdata">Posted <?php the_time('M j, Y') ?> in <?php the_category(', ') ?></p>
-    <?php if(is_single('1420') || is_single('1431')): // display for test postings only ?> 
-    
-    <?php // display as a flexslider gallery
+    <p class="postdata">Posted <?php the_time('M j, Y') ?> in <?php the_category(', ') ?></p>    
+    <?php // display attachment images as a flexslider gallery
 	
-	$attachments = get_children( array('post_parent' => get_the_ID(), 'post_type' => 'attachment', 'post_mime_type' => 'image','caption' => $attachment->post_excerpt,) );
+	$attachments = get_children(array('post_parent' => get_the_ID(), 'order' => 'ASC', 'post_type' => 'attachment', 'post_mime_type' => 'image','caption' => $attachment->post_excerpt, ));
 	
-	if (count($attachments) > 0) { // count to see if there are images ?>
+	if ($attachments) { // see if there are images attached to posting ?>
         
         <!-- Begin Slider --> 
         <div class="flexslider">
@@ -36,9 +34,8 @@
         </div>
         <!-- End Slider -->
         
-	<?php } // end count for images ?>  
+	<?php } // end see if images ?>  
            
-    <?php endif; /* end if is test postings */ ?>
     <?php the_content(''); ?> 
     <nav class="post-navigation">
         <span class="post-navigation-previous"><?php previous_post(' &laquo; %','', 'yes'); ?></span>
